5e372244743345950349a168db9053aa994fb421
sheilloputratama <sheilloggendut@gmail.com>
2025-05-26 19:02:49 +0700

    membuat method ibu dengan cetak anak

--- ibu.h
+++ ibu.h
@@ -18,3 +18,13 @@ void ibu :: tambahAnak(anak *pAnak)
 {
     daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak()
+{
+    cout << "Daftar anak dari ibu \"" << this->nama <<"\":\n";
+
+    for (auto &a : daftar_anak)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
